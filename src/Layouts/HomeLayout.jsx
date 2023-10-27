bbe5f3e8ed4cdd52da8bc4e6639d2e6b457e3b8b
anuj upadhyay <anuju760@gmail.com>
2023-10-27 17:14:34 +0530

    Added loin singup button to mside bar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,12 +1,24 @@
 import {FiMenu} from 'react-icons/fi'
 import {AiFillCloseCircle} from 'react-icons/ai'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from "react-redux"
 import Footer from '../components/footer'
 
 
 
 
+
+
 function HomeLayout({children}) {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+// for checking if user is logged in 
+const isLoggedIn = useSelector((state)=> state?. auth ?. isLoggedIn)//stae null nahi hai toh auth prsent hai auth null nahi hai toh isLogged in present hoga.
+
+// for dispalying the options acc. to role
+const role = useSelector((state)=>state?. auth?. role);//lecture name "adding sidebar buttons on sidebar"
 
     function changeWidth() {
         const drawerSide = document.getElementsByClassName('drawer-side');
@@ -21,6 +33,14 @@ function HomeLayout({children}) {
         drawerSide[0].style.width=0;
     
        
+    }
+    function handleLogout(e) {
+        e.preventDefault();
+
+        // const res = await dispatch(logout());
+        // if(res?. payload?.success) AGAR RESULT KE PAYLOAD MAIN SUCCESS AARAHA Hoga US CASE MAIN HUM LOGOUT KER DENGE
+        navigate('/')
+        
     }
 
     return(
@@ -44,9 +64,16 @@ function HomeLayout({children}) {
                             <AiFillCloseCircle size={24}/>
                         </button>
                     </li>
+
                     <li>
                         <Link to='/'>Home</Link>
                     </li>
+                    {isLoggedIn && role ==="ADMIN" && (
+                        <li>
+                            <Link to="/admin/dashboard">Admin Dashboard</Link>
+                        </li>
+                    )}
+
                     <li>
                         <Link to='/courses'>All Courses</Link>
                     </li>
@@ -56,6 +83,34 @@ function HomeLayout({children}) {
                     <li>
                         <Link to='/about'>About Us</Link>
                     </li>
+
+                    {!isLoggedIn && (
+                        <li className='absolute bottom-4 w-[90%]'>
+                        <div className='w-full flex items-center justify-center '>
+                            <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                <Link to="/login">Login</Link>
+                            </button>
+
+                            <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                <Link to="/signup">SignUp</Link>
+                            </button>
+                        </div>
+                        </li>
+                    )}
+
+{isLoggedIn && (
+                        <li className='absolute bottom-4 w-[90%]'>
+                        <div className='w-full flex items-center justify-center '>
+                            <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                <Link to="/user/profile">Profile</Link>
+                            </button>
+
+                            <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                <Link to={handleLogout}>LogOut</Link>
+                            </button>
+                        </div>
+                        </li>
+                    )}
                 </ul>
             </label>
             </div>
