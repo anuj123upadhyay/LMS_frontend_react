1a775e266545269684e19ecd888e3c5539dee954
anuj upadhyay <anuju760@gmail.com>
2023-10-28 10:53:07 +0530

    Added 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import './App.css'
 import HomeLayout from './Layouts/HomeLayout'
 import HomePage from './Pages/HomePage'
 import AboutUs from './Pages/AboutUs'
+import NotFound from './Pages/NotFound'
 
 function App() {
-  
 
-  return (
+    return (
     <>
      
        <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/about' element={<AboutUs/>}></Route>
+
+
+
+        <Route path='*' element={<NotFound/>}></Route>
+        
 
        </Routes>
    
